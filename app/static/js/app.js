47bcefc53af362a84d1669a67a1c8e09938ed446
matijaklj <matija.kljun@gmail.com>
2016-12-03 17:38:40 +0100

    angular problems

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,17 +1,23 @@
 'use strict';
 var app = angular.module('a_atendance', ['ngRoute']);
 
+
 app.config(
 	function($routeProvider) {
 		$routeProvider.
 	    when("/index", {
-	        templateUrl: "/static/templates/index.html"
+			templateUrl: "/static/templates/ucilnice.html"
 	    }).
-	    otherwise({
-	    	redirectTo : "/index"
-	    });
+		when("/ucilnica", {
+	        templateUrl: "/static/templates/ucilnica.html"
+	    }).
+		otherwise({
+	        templateUrl: "/static/templates/ucilnice.html"
+    });
 	}
 );
+
+
 
 app.config(
 	function($interpolateProvider) {
@@ -23,22 +29,87 @@ app.config(['$qProvider', function ($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
 }]);
 
-
-app.controller('indexPageController', [
-	'$scope', '$http', '$location', '$rootScope', 
+app.controller('ucilnicePageController', [
+	'$scope', '$http', '$location', '$rootScope',
 	function($scope, $http, $location, $rootScope) {
 
-	$scope.base_url =
-	  		'http://' + document.domain + ':' + location.port
-	var url = $scope.base_url + '/index'
+		$scope.displayUcilnica = function() {
+				console.log("laalal");
+				//$location.path("/ucilnica");
+		};
 
-	$scope.login =  function(user) {
-        console.log(user.username);
-        $http.post(url, { 'data': user }).then(
-			function(response) {
-				console.log(response.data.message);
-			},
-			function(response) {
-    			console.log("No success");
-  	})};
+		$scope.ucilnice = [
+		  {
+		    "ime": "P01",
+		    "opis": "velika predavalnica 1",
+			"steviloPrisotnih": 86
+		  },
+		  {
+		    "ime": "P3",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 6
+		  },
+		  {
+		    "ime": "P4",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 12
+		  },
+		  {
+		    "ime": "P5",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 15
+		  },
+		  {
+		    "ime": "P6",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 17
+		  },
+		  {
+		    "ime": "P3",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 20
+		  },
+		  {
+		    "ime": "P4",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 22
+		  },
+		  {
+		    "ime": "P5",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 12
+		  },
+		  {
+		    "ime": "P3",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 12
+		  },
+		  {
+		    "ime": "P4",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 12
+		  },
+		  {
+		    "ime": "P5",
+		    "opis": "mala predavalnica 3",
+			"steviloPrisotnih": 12
+		  }
+		];
+
+}]);
+
+app.controller('indexPageController', [
+	'$scope', '$http', '$location', '$rootScope',
+	function($scope, $http, $location, $rootScope) {
+		//$location.path("/ucilnice")
+
+		$scope.linkTo = function() {
+				$location.path("/ucilnica");
+		};
+
+		$scope.display = function() {
+				console.log("laalal");
+				//$location.path("/ucilnica");
+		};
+
 }]);
